7a847e5738ccc0a887d77544668e3d4d00f72482
unknown <rssujaykiran@gmail.com>
2025-02-08 10:42:43 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import {LoginPage,SignupPage,Home} from './routes/route.js';
+import {LoginPage,SignupPage,Home,CreateProduct} from './routes/route.js';
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/createproduct' element={<CreateProduct/>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
